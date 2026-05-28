0c6529f6a9c3e3aae7ef04b8bd1529ebe41c6e6a
gustavodesouza <gustavo.de.souza@hotmail.com>
2026-05-28 20:24:44 -0300

    feat(backend): handlerValidation

--- backend/src/main/java/com/lodesistemas/manutencoes/exception/ValidationHandler.java
+++ backend/src/main/java/com/lodesistemas/manutencoes/exception/ValidationHandler.java
@@ -1,4 +1,20 @@
 package com.lodesistemas.manutencoes.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ValidationHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handle(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(e -> erros.put(e.getField(), e.getDefaultMessage()));
+        return erros;
+    }
 }
