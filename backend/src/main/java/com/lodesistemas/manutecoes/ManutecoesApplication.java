183f9f4ad982bf940a59d97b1eea0c82f740a838
gustavodesouza <gustavo.de.souza@hotmail.com>
2026-05-28 13:23:09 -0300

    feat: starting backend config

---
+++ backend/src/main/java/com/lodesistemas/manutecoes/ManutecoesApplication.java
@@ -0,0 +1,13 @@
+package com.lodesistemas.manutecoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutecoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutecoesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lodesistemas/manutecoes/ManutecoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodesistemas.manutecoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutecoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
